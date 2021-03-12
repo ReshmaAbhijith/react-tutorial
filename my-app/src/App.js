dc98b039376e2be0c4ba96a83c668e5b3fdca7c0
Reshma Sivakumar <rsivakumar@bncnetwork.net>
2021-03-12 20:34:28 +0400

    React Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import 'font-awesome/css/font-awesome.css';
-import 'bootstrap-social/bootstrap-social.css';
-import Main from './components/MainComponent';
-import { Component } from 'react';
+import "./App.css";
+import "font-awesome/css/font-awesome.css";
+import "bootstrap-social/bootstrap-social.css";
+import Main from "./components/MainComponent";
+import { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-
   render() {
     return (
-      <div >
-         <Main/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
